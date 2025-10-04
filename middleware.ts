e48d1cbb90884ade4bb7607a5d659f4b0e7f7724
Srininas <seenu405@gmail.com>
2025-10-05 01:01:42 +0530

    updated

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,13 @@ export default clerkMiddleware();
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
+        // Allow static assets
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+
+        // ✅ Apply middleware to all API routes EXCEPT `/api/inngest`
+        '/api/(?!inngest).*',
+
+        // Apply to trpc routes
+        '/trpc(.*)',
     ],
 };
